6bd0c716c38ea1a62cf44a575dcb00ab8b3d2590
shiv bahadur <shivyadav7355@gmail.com>
2026-04-25 12:17:24 +0530

    validation handled

--- XDisplayName/src/App.jsx
+++ XDisplayName/src/App.jsx
@@ -3,26 +3,33 @@ import './App.css'
 
 function App() {
 
+  const [errors,setErrors]=useState({firstName:"",lastName:""})
   const [fullName, setFullName] = useState({
     firstName: "",
     lastName: "",
   })
-  const [displayName, setDisplayName] = useState(false);
+  const [displayName, setDisplayName] = useState("");
 
   const handleChange = (e) => {
-    console.log(e.target)
     const { name, value } = e.target
     setFullName((prev)=>({...prev,[name]:value}))
-    console.log(fullName)
   }
 
   const handleSubmit = (e) => {
+  console.log("triggered")
     e.preventDefault()
-    if (fullName.firstName && fullName.lastName) {
-      setDisplayName(true)
+    let newErrors={firstName:"",lastName:""}
+    if (!fullName.firstName.trim()) {
+      newErrors.firstName="Please fill out this field"
+    }
+    if(!fullName.lastName.trim()){
+      newErrors.lastName="Please fill out this field"
+    }
+    setErrors(newErrors);
+    if(!newErrors.firstName && !newErrors.lastName){
+      setDisplayName(fullName.firstName+" "+fullName.lastName)
     }else{
-      setDisplayName(false)
-      return
+      setDisplayName("")
     }
   }
 
@@ -32,14 +39,18 @@ function App() {
         <h1>Full Name Display</h1><br/>
         <form onSubmit={handleSubmit}>
           <label htmlFor='firstName'>First Name:</label>
-          <input type='text' name='firstName' value={fullName.firstName} id='firstName' onChange={handleChange} required/><br/>
+          <input type='text' name='firstName' value={fullName.firstName} id='firstName' onChange={handleChange}/>
+          {errors.firstName && <p style={{color:'red'}}>{errors.firstName}</p>}
+          <br/>
           <label htmlFor="lastName">Last Name:</label>
-          <input type='text' name='lastName' value={fullName.lastName} id='lastName' onChange={handleChange} required/><br/>
+          <input type='text' name='lastName' value={fullName.lastName} id='lastName' onChange={handleChange}/>
+          {errors.lastName && <p style={{color:'red'}}>{errors.lastName}</p>}
+          <br/>
           <button type='submit'>submit</button>
         </form>
         <div>
           {
-            displayName && <h2>Full Name: {fullName.firstName} {fullName.lastName}</h2>
+            displayName && <h2>Full Name: {displayName}</h2>
           }
         </div>
       </div>
